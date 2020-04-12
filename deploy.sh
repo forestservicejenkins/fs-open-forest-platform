6ab668a6e074ee62e5b17168147757f20c33c139
kilara77 <kilara77@gmail.com>
2020-04-12 15:57:35 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 echo "Workspace url echo $1"
 cd "$1"
 pwd
-        source /var/Jenkins/openforest.sh
+        source /home/Jenkins/openforest.sh
         sudo chown -R Jenkins:Jenkins frontend
         sudo chown -R Jenkins:Jenkins server
         chmod -R 777 frontend
